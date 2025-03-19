cdb76ab714a0839a3057c439fdca0c08b51fbce5
Afterimages <zy13682943183@163.com>
2025-03-19 23:36:03 +0800

    第四版：用户业务层封装

--- server.go
+++ server.go
@@ -54,16 +54,9 @@ func (this *Server) Broadcast(user *User, msg string) {
 // 处理链接事务
 func (this *Server) Handle(conn net.Conn) {
 	//...当前链接的业务
-	fmt.Println("链接建立成功")
-	user := NewUser(conn)
-
-	// 用户上线，将用户加入到onlineMap中
-	this.mapLock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.mapLock.Unlock()
-
-	// 广播当前用户上线消息
-	this.Broadcast(user, "已上线")
+	// fmt.Println("链接建立成功")
+	user := NewUser(conn, this)
+	user.Online()
 
 	// 接收客户端发送的消息
 	go func() {
@@ -71,7 +64,7 @@ func (this *Server) Handle(conn net.Conn) {
 		for {
 			n, err := conn.Read(buf)
 			if n == 0 {
-				this.Broadcast(user, "下线")
+				user.Offline()
 				return
 			}
 			if err != nil && err != io.EOF {
@@ -82,8 +75,8 @@ func (this *Server) Handle(conn net.Conn) {
 			// 提取用户的消息（去除'\n'）
 			msg := string(buf[:n-1])
 
-			// 将得到的消息进行广播
-			this.Broadcast(user, msg)
+			// 用户针对msg进行消息处理
+			user.DoMessage(msg)
 
 		}
 	}()
--- user.go
+++ user.go
@@ -7,10 +7,12 @@ type User struct {
 	Addr string
 	C    chan string
 	conn net.Conn
+
+	server *Server
 }
 
 // 创建一个用户的API
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 
 	user := &User{
@@ -18,6 +20,8 @@ func NewUser(conn net.Conn) *User {
 		Addr: userAddr,
 		C:    make(chan string),
 		conn: conn,
+
+		server: server,
 	}
 	// 启动监听当前user channel消息的goroutine
 	go user.ListenMessager()
@@ -31,4 +35,31 @@ func (this *User) ListenMessager() {
 		msg := <-this.C
 		this.conn.Write([]byte(msg + "\n"))
 	}
+}
+
+// 用户的上线业务
+func (this *User) Online() {
+	// 用户上线，将用户加入到onlineMap中
+	this.server.mapLock.Lock()
+	this.server.OnlineMap[this.Name] = this
+	this.server.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	this.server.Broadcast(this, "已上线")
+}
+
+// 用户的下线业务
+func (this *User) Offline() {
+	// 用户下线，将用户从onlinemap中删除
+	this.server.mapLock.Lock()
+	delete(this.server.OnlineMap, this.Name)
+	this.server.mapLock.Unlock()
+
+	// 广播当前用户下线消息
+	this.server.Broadcast(this, "下线")
+}
+
+// 用户处理消息的业务
+func (this *User) DoMessage(msg string) {
+	this.server.Broadcast(this, msg)
 }
