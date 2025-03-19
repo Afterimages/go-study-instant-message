819ab384bb2b7a5d0405c6f3968717bda722bd1b
Afterimages <zy13682943183@163.com>
2025-03-19 15:23:40 +0800

    第二版：用户上线功能

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 func main() {
 	// TODO: Implement the main function
 	// go mod init + GitHub项目地址 解决找不到newserver问题
-	// 运行：go run server.go mian.go 启动服务端，然后再postman中向localhost:8888发送请求, 成功
+	// 运行：go run server.go main.go 启动服务端，然后再postman中向localhost:8888发送请求, 成功
 	server := NewServer("127.0.0.1", 8888)
 	server.Start()
+	// go build -o server.exe main.go server.go user.go
+	// server
+	// 或 go run server.go main.go user.go
+	// 然后新开一个终端 chcp 65001 换行 ncat localhost 8888
 }
--- server.go
+++ server.go
@@ -1,52 +1,96 @@
+// 快速变量类型和变量名对齐：shift+alt+f
 package main
 
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	// 在线用户的列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// 消息广播的channel
+	Message chan string
 }
 
 // 创建一个server的接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
-// 启动服务器的接口
-func (this *Server) Start() {
-	// socket listen
-	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
-
-	if err != nil {
-		fmt.Println("net.Listen err:", err)
-		return
-	}
-
-	// close listen socket
-	defer listener.Close()
-
+// 监听Message广播消息channel的goroutine，一旦有消息，就发送给全部在线的User
+func (this *Server) ListenMessager() {
 	for {
-		//accept
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("listener accept err:", err)
-			continue
-		}
+		msg := <-this.Message
 
-		//do handle
-		go this.Handle(conn)
+		// 将msg发送给全部的在线User
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
 	}
+}
+
+// 广播消息的方法
+func (this *Server) Broadcast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
 }
 
 // 处理链接事务
 func (this *Server) Handle(conn net.Conn) {
 	//...当前链接的业务
 	fmt.Println("链接建立成功")
+	user := NewUser(conn)
+
+	// 用户上线，将用户加入到onlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	this.Broadcast(user, "已上线")
+
+	// 当前handle阻塞
+	select {}
+}
+
+// 启动服务器的接口
+func (this *Server) Start() {
+	// socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+	fmt.Printf("服务器启动成功，在[%s:%d]监听...\n", this.Ip, this.Port)
+	defer listener.Close()
+
+	// 启动监听Message channel的goroutine
+	go this.ListenMessager()
+
+	for {
+		// accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept err:", err)
+			continue
+		}
+
+		// do handle
+		go this.Handle(conn)
+	}
 }
