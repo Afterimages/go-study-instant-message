da2e3695d694b17942f846ebbada8d45ca3fc87b
Afterimages <zy13682943183@163.com>
2025-03-21 00:43:37 +0800

    第九版-2：解析命令行

--- clients/client.go
+++ clients/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -32,10 +33,23 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+var serverIp string
+var serverPort int
+
+// ./client -ip 127.0.0.1 -port 8888
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器ip地址(默认是127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口(默认是8888)")
+}
+
 func main() {
 	// 如果同一个目录下有两个main，只能分别build才不会引起冲突。
 	// 否则最好分开成两个目录
-	client := NewClient("127.0.0.1", 8888)
+
+	// 命令行解析
+	flag.Parse()
+
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>>>连接服务器失败...")
 		return
