e0d31d37b7541a8c1eaec1fb9b895e2effca1cab
Afterimages <zy13682943183@163.com>
2025-03-21 10:49:44 +0800

    第九版-4：更新用户名

--- clients/client.go
+++ clients/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -54,6 +56,25 @@ func (client *Client) menu() bool {
 	}
 }
 
+func (client *Client) UpdateName() bool {
+	fmt.Println(">>>>请输入用户名")
+	fmt.Scanln(&client.Name)
+
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write.err:", err)
+		return false
+	}
+	return true
+}
+
+// 处理server回应的消息，直接显示到标准输出即可
+func (client *Client) DealResponse() {
+	// 一旦client.conn有数据，就直接copy到stdout标准输出上，永久阻塞监听
+	io.Copy(os.Stdout, client.conn)
+}
+
 var serverIp string
 var serverPort int
 
@@ -79,7 +100,7 @@ func (client *Client) Run() {
 			break
 		case 3:
 			// 更新用户名
-			fmt.Println("更新用户名选择...")
+			client.UpdateName()
 			break
 		}
 	}
@@ -97,6 +118,9 @@ func main() {
 		fmt.Println(">>>>连接服务器失败...")
 		return
 	}
+
+	// 单独开启一个goroutine去处理server的回执消息
+	go client.DealResponse()
 
 	fmt.Println(">>>>连接服务器成功...")
 
